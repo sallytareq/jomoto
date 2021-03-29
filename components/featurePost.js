f8cf4095a6767e6884f88b6e3e6a50d5ab19f27d
sallytareq <sally.ammous@gmail.com>
2021-03-30 00:57:38 +0300

    desktop home

---
+++ components/featurePost.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link'
+function FeaturePost(props) {
+
+    return (
+        <div className="featurePost__container" dir="rtl">
+            <Link href="/post/[slug]" as={`/post/${props.post.slug}`}>
+                <img src={props.post.feature_image} className="featurePost__img" />
+            </Link>
+            <div className="featurePost__info">
+                <Link href="/post/[slug]" as={`/post/${props.post.slug}`}>
+                    <h2>{props.post.title}</h2>
+                </Link>
+                <div className="featurePost__text" dangerouslySetInnerHTML={{ __html: props.post.html }}></div><span>...</span>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturePost
