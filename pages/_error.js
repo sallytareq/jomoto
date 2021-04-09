52566ec27466944e9a3b5c1abb952f8abd7c68f1
sallytareq <sally.ammous@gmail.com>
2021-04-10 00:51:35 +0300

    adjust imports

---
+++ pages/_error.js
@@ -0,0 +1,15 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+export default function Error(props) {
+    return (
+      <div className={styles.container}>
+        <Header home={true} />
+        <main className={styles.main}>
+  
+        </main>
+        <Footer />
+      </div>
+    )
+  }
+  
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,17 +1,16 @@
-// import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-
-import ReactDOM from 'react-dom';
-import Footer from '../components/footer'
-import Header from '../components/header'
-import cloudinary from 'cloudinary'
-import Link from 'next/link'
 import { useState } from 'react';
-import { useEffect } from 'react'
+
+import Link from 'next/link'
+
+import cloudinary from 'cloudinary'
 import IconButton from '@material-ui/core/IconButton';
 import NavigateNextRoundedIcon from '@material-ui/icons/NavigateNextRounded';
 import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
-const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUD_NAME } = process.env
 
+import Footer from '../components/footer'
+import Header from '../components/header'
+
+const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUD_NAME } = process.env
 
 export async function getServerSideProps() {
 
@@ -23,8 +22,6 @@ export async function getServerSideProps() {
 
     const data = await cloudinary.v2.api.resources({ max_results: 21 })
     const images = data.resources
-
-    // console.log("IMAGES",images);
 
     if (!images) {
         return {
@@ -41,7 +38,7 @@ export async function getServerSideProps() {
 }
 
 export default function Gallery({ images }) {
-    // console.log(images[0]);
+
     const [selected, setSelected] = useState(0)
 
     const nextImage = () => {
