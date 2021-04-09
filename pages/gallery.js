6ddad75119e969da2a2deb5985f9c14032f2674b
sallytareq <sally.ammous@gmail.com>
2021-04-10 00:12:52 +0300

    next and prev buttons

--- pages/gallery.js
+++ pages/gallery.js
@@ -6,6 +6,7 @@ import Header from '../components/header'
 import cloudinary from 'cloudinary'
 import { useState } from 'react';
 import { useEffect } from 'react'
+import IconButton from '@material-ui/core/IconButton';
 import NavigateNextRoundedIcon from '@material-ui/icons/NavigateNextRounded';
 import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
 const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUD_NAME } = process.env
@@ -70,9 +71,13 @@ export default function Gallery({ images }) {
             <Header home={false} />
             <main>
                 <div className='gallery__hero'>
-                    <NavigateBeforeRoundedIcon onClick={prevImage} />
+                    <IconButton>
+                        <NavigateBeforeRoundedIcon onClick={prevImage} />
+                    </IconButton>
                     <img src={selected ? images[selected].secure_url : images[0].secure_url} alt='selected' className='gallery__selected' />
-                    <NavigateNextRoundedIcon onClick={nextImage} />
+                    <IconButton>
+                        <NavigateNextRoundedIcon onClick={nextImage} />
+                    </IconButton>
                 </div>
                 <div className='gallery'>
                     {images.map((image, index) => {
