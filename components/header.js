b6959f021388946659eb8c602ddfb786ee81e1a8
sallytareq <sally.ammous@gmail.com>
2021-04-17 17:33:20 +0300

    header change

--- components/header.js
+++ components/header.js
@@ -33,18 +33,19 @@ function Header(props) {
   return (
     <>
       <Head>
+        <script data-ad-client="ca-pub-2321742861441338" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <title>JoMoto</title>
         <link rel="icon" href="/icon1.ico" />
       </Head>
-      <header className={props.home ? 'header__home' : 'header'}>
+      <header className={props.home ? 'header__home' : 'header'} dir="rtl">
         <div className={mobile ? 'nav__hidden' : 'navbar__nav'}>
           <Navbar className={props.home ? (scroll ? 'navbar__normal' : 'navbar__top') : 'navbar__normal'} variant={props.home ? (scroll ? 'dark' : 'light') : 'dark'}>
-            <Navbar.Brand className="navbar__brand" href="/">JoMoto Blog</Navbar.Brand>
+            <Navbar.Brand className="navbar__brand" href="/"><img className='logo' src='/icon1.ico' /></Navbar.Brand>
             <Nav>
-              <Nav.Link className="navbar__navitem" href="/">Home</Nav.Link>
-              <Nav.Link className="navbar__navitem" href="/posts">Posts</Nav.Link>
-              <Nav.Link className="navbar__navitem" href="/gallery">Gallery</Nav.Link>
-              <Nav.Link className="navbar__navitem" href="/about">About</Nav.Link>
+              <Nav.Link className="navbar__navitem" href="/">الرئيسية</Nav.Link>
+              <Nav.Link className="navbar__navitem" href="/posts">المنشورات</Nav.Link>
+              <Nav.Link className="navbar__navitem" href="/gallery">الإستديو</Nav.Link>
+              <Nav.Link className="navbar__navitem" href="/about">من أنا</Nav.Link>
             </Nav>
           </Navbar>
         </div>
